1c1c259045cd868f61d82ddfba5fa6c249389ea8
Cesar <cesar@cesar.wtf>
2024-02-02 14:53:20 +0100

    New project structure proposal

--- models/counters.sql
+++ models/counters.sql
@@ -1,24 +1,9 @@
-SELECT
-  COUNT(DISTINCT e.user_id) AS unique_users,
-  COUNT(DISTINCT e.workspace_id) AS unique_workspaces,
-  c.total_users,
-  c.total_workspaces,
-  ROUND((COUNT(DISTINCT e.user_id) / c.total_users)::numeric * 100.0, 2)::float AS user_ratio_percentage,
-  ROUND((COUNT(DISTINCT e.workspace_id) / c.total_workspaces)::numeric * 100.0, 2)::float AS workspace_ratio_percentage,
-FROM
-  {{ref('events_enriched')}} AS e
-CROSS JOIN
-  (SELECT
-     SUM(total_users) AS total_users,
-     SUM(total_workspaces) AS total_workspaces
-   FROM
-     (
-        SELECT
-            COUNT(DISTINCT u.id) AS total_users,
-            COUNT(DISTINCT u.workspace_id) AS total_workspaces
-        FROM {{ref('base/users')}} AS u
-        where u.activated_ever = 1
-      )) AS c
-GROUP BY
-  c.total_users,
-  c.total_workspaces
+select clean_countries[1] as main_country,
+       count(*) as count,
+       sum(case when type = 'MOVIE' then 1 else null end) as movies,
+       sum(case when type = 'SHOW' then 1 else null end) as shows
+from {{ ref('titles') }}
+where clean_countries[1] != ''
+group by 1
+order by 2 desc
+limit 20
