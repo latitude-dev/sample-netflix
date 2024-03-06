96eb9aa7f283405864f7d82d60a7d9b0595bd11a
Cesar <cesar@cesar.wtf>
2024-03-06 11:59:49 +0100

    First working version

--- queries/titles.sql
+++ queries/titles.sql
@@ -1,3 +1,7 @@
-select *
+select *,
+       string_to_array(ltrim(rtrim(production_countries, ']'), '['), ', ') as clean_countries
 from read_csv_auto('~/Dev/latitude/template/.latitude/app/static/latitude/queries/netflix.csv')
-limit 10
+{#if param('start_year') && param('end_year')}
+    where release_year between { interpolate(param('start_year')) } and { interpolate(param('end_year')) }
+{/if}
+order by imdb_score desc
