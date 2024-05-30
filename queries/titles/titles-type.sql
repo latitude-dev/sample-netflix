001ed85eb13228fc596eba55b4ae86ad4fac34b7
Carlos Sansón <carlos@latitude.so>
2024-05-30 15:10:39 +0200

    fix: use relative paths

--- queries/titles/titles-type.sql
+++ queries/titles/titles-type.sql
@@ -1,5 +1,5 @@
 select type,
        count(distinct id) as count_titles,
-from { ref('titles/titles') }
+from { ref('titles') }
 group by 1
 order by 1 asc
--- queries/titles/titles-year-type.sql
+++ queries/titles/titles-year-type.sql
@@ -2,6 +2,6 @@ select release_year,
        count(distinct id) as count_total,
        count(distinct case when type='SHOW' then id else null end) as count_shows,
        count(distinct case when type='MOVIE' then id else null end) as count_movies,
-from { ref('titles/titles') }
+from { ref('titles') }
 group by 1
 order by 1 asc
